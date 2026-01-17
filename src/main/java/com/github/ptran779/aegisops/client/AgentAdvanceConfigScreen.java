4a3d14ddb74c10c2e48a8c455e57473f28eff2b1
ptran779 <phong6900user@gmail.com>
2026-01-17 14:19:30 -0600

    add all special items' tooltip

--- src/main/java/com/github/ptran779/aegisops/client/AgentAdvanceConfigScreen.java
+++ src/main/java/com/github/ptran779/aegisops/client/AgentAdvanceConfigScreen.java
@@ -40,7 +40,6 @@ public class AgentAdvanceConfigScreen extends AbstractContainerScreen<AgentAdvan
         Set<String> skinNames = SkinManager.getAllSkin(agent.getFemale());
 
         DropDownWidget skinDropdown = new DropDownWidget(this.leftPos + 64, this.topPos + 27,74,11,
-        DropDownWidget skinDropdown = new DropDownWidget(this.leftPos + 63, this.topPos + 28,75,12,
             skinNames, agent.getSkin(),
             selected -> {
                 PacketHandler.CHANNELS.sendToServer(new ChangeSkinPacket(agent.getId(), selected)); // server sync
